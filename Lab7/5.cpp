11b61af3eb83ed861f74e770663ff61bbfef8064
Pakorn Thanaprachanon <77424125+T-Pakorn@users.noreply.github.com>
2021-03-16 00:22:26 +0700

    Add Lab7

---
+++ Lab7/5.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>    
+int main()
+{ 
+	int A;
+	int B;
+
+
+    printf("Enter Number A: ");
+    scanf("%d",&A);
+    printf("Enter Number B: ");
+    scanf("%d",&B);
+    printf("A %% B = %d",A%B);
+    
+    return 0;
+}
